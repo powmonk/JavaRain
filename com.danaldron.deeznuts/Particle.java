0fc34ac8cda5baf1707022500216157495f65139
powmonk <dan@Aldron.co.uk>
2018-03-23 16:31:10 +0000

    Create Particle.java

---
+++ com.danaldron.deeznuts/Particle.java
@@ -0,0 +1,35 @@
+package com.danaldron.deeznuts;
+
+class Particle                                                                                                                                            
+{                                                                                                                                                         
+	private int partX;                                                                                                                                
+	private int partY;        
+	private int partSway;
+	
+	public void setX(int x){                                                                                                                              
+		partX = x;                                                                                                                                        
+	}
+	
+	public void setY(int y){                                                                                                                           
+		partY = y;                                                                                                                                        
+	}                                                                                                                                                     
+
+	public void setSway(int sway){                                                                                                                                    
+		partSway = sway;                                                                                                                                     
+	}                                                                                                                                                     
+	
+	public int getX(){                                                                                                                                    
+		return partX;                                                                                                                                     
+	}
+	
+	public int getY(){                                                                                                                                    
+		return partY;                                                                                                                                     
+	}                                                                                                                                                     
+	                                                                                                                                                      
+	public int getSway(){                                                                                                                                    
+		return partSway;                                                                                                                                     
+	}
+	                                                                                                                                                      
+}                                                                                                                                                         
+                                                                                                                                                          
+                                                                    
